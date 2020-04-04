d54b4919eb3fa2746add294563289cc76155f564
tommy8808 <tommyguy8808@gmail.com>
2020-04-04 22:31:43 +0900

    Posts 도메인 CRUD 추가 JPA Auditing(생성시간, 수정시간 자동추가) 추가

---
+++ src/main/java/com/runtoyou/api/service/PostsService.java
@@ -0,0 +1,37 @@
+package com.runtoyou.api.service;
+
+import com.runtoyou.api.domain.posts.Posts;
+import com.runtoyou.api.domain.posts.PostsRepository;
+import com.runtoyou.api.web.dto.PostsResponseDto;
+import com.runtoyou.api.web.dto.PostsSaveRequestDto;
+import com.runtoyou.api.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
