6189cbee0b880b05760da01730aeeb14fe79b7a4
tommy8808 <tommy8808@gmail.com>
2020-04-05 15:56:52 +0900

    리스트 조회, 삭제 api 추가

--- src/main/java/com/runtoyou/api/web/PostsApiController.java
+++ src/main/java/com/runtoyou/api/web/PostsApiController.java
@@ -1,11 +1,14 @@
 package com.runtoyou.api.web;
 
 import com.runtoyou.api.service.PostsService;
+import com.runtoyou.api.web.dto.PostsListResponseDto;
 import com.runtoyou.api.web.dto.PostsResponseDto;
 import com.runtoyou.api.web.dto.PostsSaveRequestDto;
 import com.runtoyou.api.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -13,6 +16,10 @@ public class PostsApiController {
 
     private final PostsService postsService;
 
+    @GetMapping("/api/v1/posts/")
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsService.findAllDesc();
+    }
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
@@ -26,5 +33,11 @@ public class PostsApiController {
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
     }
 }
--- src/test/java/com/runtoyou/api/web/PostsApiControllerTest.java
+++ src/test/java/com/runtoyou/api/web/PostsApiControllerTest.java
@@ -98,4 +98,34 @@ public class PostsApiControllerTest {
         assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
         assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
     }
+
+    @Test
+    public void Posts_삭제된다() throws Exception {
+        //given
+        Posts savedPosts = postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        Long deleteId = savedPosts.getId();
+        String expectedTitle ="title2";
+        String expectedContent = "content2";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(expectedTitle)
+                .content(expectedContent)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + deleteId;
+
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        //when
+        restTemplate.delete(url);
+
+        //then
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.isEmpty()).isEqualTo(true);
+    }
 }
