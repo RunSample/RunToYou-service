e8b8143d2731e12fae3a8ed9b5e5153252e44016
tommy8808 <tommyguy8808@gmail.com>
2020-06-07 22:03:24 +0900

    corsconfig 추가

--- src/main/java/com/runtoyou/api/config/WebConfig.java
+++ src/main/java/com/runtoyou/api/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.runtoyou.api.config.auth.LoginUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -17,4 +18,12 @@ public class WebConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginUserArgumentResolver);
     }
+
+    // 아래 설정을 추가하면 cors 주소를 한정할 수 있다. 지금은 개발 단계이므로 모든 주소를 풀어도 되지만
+    // 실제 서비스를 공개했을 때는 정식 요청 주소만 가능한 서비스가 필요할 것이다.
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:3000");
+//    }
 }
